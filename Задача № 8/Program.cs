0898092e9c021cfd06ab962302df0c55a9d43a66
danil <jardus@bk.ru>
2022-10-16 18:02:40 +0300

    Задача № 8 к Семинару 1

---
+++ Задача № 8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число - ");
+int n = int.Parse(Console.ReadLine());
+if (n > 0)
+for (int i = 1; i <= n; i++){
+    if (i % 2 == 0){
+        Console.WriteLine(i);
+        }
+    }
+if (n < 0) 
+for (int i = 1; i >= n; i--){
+if (i % 2 == 0){
+    Console.WriteLine(i);
+        }   
+    }
